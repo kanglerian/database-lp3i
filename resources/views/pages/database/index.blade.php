1b7a5e0f9a8068396d43d0266de80e6a09e97057
Lerian Febriana <kanglerian@MJLQ2s-MacBook-Pro.local>
2023-06-07 11:31:38 +0700

    eee

--- resources/views/pages/database/index.blade.php
+++ resources/views/pages/database/index.blade.php
@@ -167,6 +167,7 @@
     }
 
     const copyRecord = (name, phone, school, year, source) => {
+        alert(`${typeof(school) school}`);
         var contentSource = '';
         switch (source) {
             case "1":
