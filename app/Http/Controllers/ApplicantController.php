e1cea3ad6cfb84059563906973c0204fa082b585
Lerian Febriana <kanglerian@MJLQ2s-MacBook-Pro.local>
2023-06-19 11:47:45 +0700

    cek

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Applicant;
 use App\Models\User;
+use Illuminate\Database\QueryException;
 
 class ApplicantController extends Controller
 {
@@ -74,31 +75,38 @@ class ApplicantController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'phone' => ['required', 'string', 'max:15', 'min:10'],
-            'school' => ['required', 'string', 'max:255'],
-            'year' => ['required', 'string', 'digits:4'],
-            'source' => ['required', 'string', 'not_in:Pilih sumber'],
-            'status' => ['required', 'string', 'not_in:Pilih status'],
-            'nik_user' => ['string', 'not_in:Pilih presenter'],
-            'program' => ['string', 'not_in:Pilih program'],
-            'isread' => ['string'],
-        ]);
-        $data = [
-            'name' => $request->input('name'),
-            'phone' => $request->input('phone'),
-            'school' => $request->input('school'),
-            'year' => $request->input('year'),
-            'source' => $request->input('source'),
-            'source' => $request->input('source'),
-            'status' => $request->input('status'),
-            'nik_user' => $request->input('nik_user'),
-            'program' => $request->input('program'),
-            'isread' => '0',
-        ];
-        Applicant::create($data);
-        return back()->with('message', 'Data aplikan berhasil ditambahkan!');
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'phone' => ['required', 'string', 'max:15', 'min:10'],
+                'school' => ['required', 'string', 'max:255'],
+                'year' => ['required', 'string', 'digits:4'],
+                'source' => ['required', 'string', 'not_in:Pilih sumber'],
+                'status' => ['required', 'string', 'not_in:Pilih status'],
+                'nik_user' => ['string', 'not_in:Pilih presenter'],
+                'program' => ['string', 'not_in:Pilih program'],
+                'isread' => ['string'],
+            ]);
+            $data = [
+                'name' => $request->input('name'),
+                'phone' => $request->input('phone'),
+                'school' => $request->input('school'),
+                'year' => $request->input('year'),
+                'source' => $request->input('source'),
+                'source' => $request->input('source'),
+                'status' => $request->input('status'),
+                'nik_user' => $request->input('nik_user'),
+                'program' => $request->input('program'),
+                'isread' => '0',
+            ];
+            Applicant::create($data);
+            return back()->with('message', 'Data aplikan berhasil ditambahkan!');
+        } catch (QueryException $exception) {
+            if ($exception->getCode() == 23000) {
+                $errorMessage = 'Terjadi duplikat data.';
+                return back()->with('error', $errorMessage);
+            }
+        }
     }
 
     /**
--- resources/views/pages/database/create.blade.php
+++ resources/views/pages/database/create.blade.php
@@ -7,6 +7,15 @@
 
     <div class="py-5">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if (session('error'))
+                <div id="alert" class="mx-2 mb-4 flex items-center p-4 mb-4 bg-red-400 text-white rounded-lg"
+                    role="alert">
+                    <i class="fa-solid fa-circle-exclamation"></i>
+                    <div class="ml-3 text-sm font-medium">
+                        {{ session('error') }}
+                    </div>
+                </div>
+            @endif
             @if (session('message'))
                 <div id="alert" class="mx-2 mb-4 flex items-center p-4 mb-4 bg-emerald-400 text-white rounded-lg"
                     role="alert">
