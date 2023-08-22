560336f4733776388880aeabb609a82401dd142b
kanglerian <kanglerian@gmail.com>
2023-08-22 11:33:07 +0700

    favicon landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    
     <!-- Fonts -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon/favicon.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
