8525ebf254ea48b280c500dc907b7909c3471914
kanglerian <kanglerian@gmail.com>
2023-09-04 08:32:32 +0700

    store

--- app/Http/Controllers/API/ApplicantController.php
+++ app/Http/Controllers/API/ApplicantController.php
@@ -39,6 +39,7 @@ class ApplicantController extends Controller
                 'name' => $request->input('name'),
                 'phone' => strpos($request->input('phone'), '0') === 0 ? '62' . substr($request->input('phone'), 1) : $request->input('phone'),
                 'pmb' => $request->input('pmb'),
+                'programtype_id' => $request->input('programtype_id'),
                 'source_id' => 1,
                 'status' => 1,
                 'isread' => '0',
