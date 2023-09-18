19f86e145c7d1e286730ab0bcd42f02228a59b61
kanglerian <kanglerian@gmail.com>
2023-09-18 14:08:32 +0700

    axios

--- resources/views/pages/profile/edit.blade.php
+++ resources/views/pages/profile/edit.blade.php
@@ -46,7 +46,7 @@
                         <div>
                             <div>
                                 <div class="relative z-0 w-full mb-6 group">
-                                    <input type="text" name="name" id="name" value="{{ $user->name }}"
+                                    <input type="text" name="name" id="nameuser" value="{{ $user->name }}"
                                         class="@error('name') border-red-500 @enderror block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                         placeholder=" " required />
                                     <div class="text-sm text-gray-700 mt-3">
