3a029c1d04c411076a489ce509ecdddaa10fc217
kanglerian <kanglerian@gmail.com>
2023-10-11 16:53:28 +0700

    coba modal

--- resources/views/pages/database/show/chat.blade.php
+++ resources/views/pages/database/show/chat.blade.php
@@ -105,3 +105,129 @@
     }
     getChats();
 </script>
+
+<script>
+    const dataModal = (button) => {
+        const modalTarget = button.dataset.modalTarget;
+        let status = document.getElementById(modalTarget);
+        let url = "{{ route('histories.store') }}";
+        document.getElementById('title_form').innerText = `Tambah Data Riwayat`;
+        document.getElementById('title').value = '';
+        document.getElementById('date').value = '';
+        document.getElementById('result').value = '';
+        document.getElementById('report').value = '';
+        document.getElementById('formButton').innerText = 'Simpan';
+        document.getElementById('formModal').setAttribute('action', url);
+
+        const elementsToRemove = document.querySelectorAll('[name="_method"]');
+        if (elementsToRemove.length > 0) {
+            elementsToRemove.forEach((element) => {
+                element.remove();
+            });
+        } else {
+            console.log("No elements found with the specified name.");
+        }
+        status.classList.toggle('hidden');
+    }
+
+    const editModal = (button) => {
+        const modalTarget = button.dataset.modalTarget;
+        const id = button.dataset.id;
+        const title = button.dataset.title;
+        const date = button.dataset.date;
+        const result = button.dataset.result;
+        const report = button.dataset.report;
+        let url = "{{ route('histories.update', ':id') }}".replace(':id', id);
+        let status = document.getElementById(modalTarget);
+        document.getElementById('title_form').innerText = `Edit Data Riwayat ${title}`;
+        document.getElementById('title').value = title;
+        document.getElementById('date').value = date;
+        document.getElementById('result').value = result;
+        document.getElementById('report').value = report;
+        document.getElementById('formButton').innerText = 'Simpan perubahan';
+        document.getElementById('formModal').setAttribute('action', url);
+        let csrfToken = document.createElement('input');
+        csrfToken.setAttribute('type', 'hidden');
+        csrfToken.setAttribute('name', '_token');
+        csrfToken.setAttribute('value', '{{ csrf_token() }}');
+        formModal.appendChild(csrfToken);
+
+        let methodInput = document.createElement('input');
+        methodInput.setAttribute('type', 'hidden');
+        methodInput.setAttribute('name', '_method');
+        methodInput.setAttribute('value', 'PATCH');
+        formModal.appendChild(methodInput);
+
+        status.classList.toggle('hidden');
+    }
+
+    const deleteModal = (item) => {
+        let id = item.dataset.id;
+        if (confirm(`Apakah kamu yakin akan menghapus data?`)) {
+            $.ajax({
+                url: `https://api.politekniklp3i-tasikmalaya.ac.id/history/delete/${id}`,
+                type: 'POST',
+                success: function(response) {
+                    location.reload();
+                },
+                error: function(xhr, status, error) {
+                    alert('Error deleting record');
+                }
+            })
+        }
+    }
+</script>
+
+
+<div class="fixed inset-0 flex items-center justify-center z-50 hidden" id="dataModal">
+    <div class="fixed inset-0 bg-black opacity-50"></div>
+    <div class="fixed inset-0 flex items-center justify-center">
+        <div class="w-full md:w-1/2 relative bg-white rounded-lg shadow mx-5">
+            <div class="flex items-start justify-between p-4 border-b rounded-t">
+                <h3 class="text-xl font-semibold text-gray-900" id="title_form">
+                    Tambah Data Riwayat
+                </h3>
+                <button type="button" onclick="dataModal(this)" data-modal-target="dataModal"
+                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center"
+                    data-modal-hide="defaultModal">
+                    <i class="fa-solid fa-xmark"></i>
+                </button>
+            </div>
+            <form method="POST" action="{{ route('histories.store') }}" id="formModal">
+                @csrf
+                <div class="p-4 space-y-6">
+                    <div>
+                        <label class="block mb-2 text-sm font-medium text-gray-900">Judul Riwayat</label>
+                        <input type="text" id="title" name="title" placeholder="Isi judul riwayat disini.."
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            required>
+                        <input type="hidden" name="phone" value="{{ $user->phone }}">
+                    </div>
+                    <div>
+                        <label class="block mb-2 text-sm font-medium text-gray-900">Tanggal</label>
+                        <input type="date" id="date" name="date"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            required>
+                    </div>
+                    <div>
+                        <label class="block mb-2 text-sm font-medium text-gray-900">Isi Pesan</label>
+                        <textarea name="result" id="result" cols="30" rows="5" placeholder="Isi pesan disini..."
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            required></textarea>
+                    </div>
+                    <div>
+                        <label class="block mb-2 text-sm font-medium text-gray-900">Hasil</label>
+                        <input type="text" id="report" name="report" placeholder="Tulis hasilnya disini"
+                            class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500">
+                    </div>
+                </div>
+                <div class="flex items-center p-4 space-x-2 border-t border-gray-200 rounded-b">
+                    <button type="submit" id="formButton"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Simpan</button>
+                    <button type="button" data-modal-target="dataModal" onclick="dataModal(this)"
+                        class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Batal</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
