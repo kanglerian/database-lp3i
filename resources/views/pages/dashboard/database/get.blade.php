f7c992dc05133d0da609b279e5a93a189cd39794
kanglerian <kanglerian@gmail.com>
2023-11-30 16:08:22 +0700

    local

--- resources/views/pages/dashboard/database/get.blade.php
+++ resources/views/pages/dashboard/database/get.blade.php
@@ -2,7 +2,7 @@
     const getDatabases = async () => {
         await axios.get(apiDashboard)
             .then((res) => {
-                document.getElementById('database_count').innerText = parseInt(res.data.database_count).toFixed();
+                document.getElementById('database_count').innerText = parseInt(res.data.database_count).toLocaleString('id-ID');
                 document.getElementById('schoolarship_count').innerText = res.data.schoolarship_count;
                 document.getElementById('applicant_count').innerText = res.data.applicant_count;
                 document.getElementById('enrollment_count').innerText = res.data.enrollment_count;
