91b3ef198be6bc623214430d081d905346e3af39
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-06-19 15:06:49 +0700

    identity

--- resources/views/pages/question/scholarship/index.blade.php
+++ resources/views/pages/question/scholarship/index.blade.php
@@ -33,6 +33,8 @@
     </x-slot>
 
     <div class="py-10">
+        <input type="hidden" id="identity_val" value="{{ Auth::user()->identity }}">
+        <input type="hidden" id="role_val" value="{{ Auth::user()->role }}">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-5">
             @if (session('message'))
                 <div id="alert" class="mx-2 flex items-center p-4 mb-4 bg-emerald-400 text-white rounded-2xl"
@@ -128,7 +130,6 @@
         <script>
             let dataTableInstance;
             let dataTableInitialized = false;
-            let pmbVal = document.getElementById('change_pmb').value;
             let url = `/api/applicants/scholarships?pmbVal=${pmbVal}`;
             let dataScholarship;
         </script>
