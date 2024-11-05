7fea207d6be6ec200fecfa69ece57f6593238d26
Lerian Febriana <kanglerian@gmail.com>
2024-11-05 19:16:07 +0700

    cobaa 2

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -1412,9 +1412,6 @@ class ApplicantController extends Controller
                             $this->update_data($studentDataPhone, $applicants, $i, $phone, $school, $gender, $identityUser, $come, $kip, $known, $program);
                         } else if ($studentDataPhone->is_applicant == 1 && $studentDataPhone->schoolarship == 1) {
                             $this->update_data($studentDataPhone, $applicants, $i, $phone, $school, $gender, $identityUser, $come, $kip, $known, $program);
-                        } else {
-
-                            $this->update_data_duplicate($studentDataPhone, $applicants, $i, $identityUser);
                         }
                     } else {
                         $studentData = Applicant::where('identity', $applicants[$i][1])->first();
@@ -1429,6 +1426,8 @@ class ApplicantController extends Controller
                                 } else {
                                     $this->update_data($studentData, $applicants, $i, $phone, $school, $gender, $identityUser, $come, $kip, $known, $program, );
                                 }
+                            } else if ($studentDataPhone->is_applicant == 1 && $studentDataPhone->schoolarship == 1) {
+                                $this->update_data($studentDataPhone, $applicants, $i, $phone, $school, $gender, $identityUser, $come, $kip, $known, $program);
                             }
                         } else {
                             $studentPhoneDup = Applicant::where('phone', $phone)->first();
